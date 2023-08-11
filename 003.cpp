06852984ce5af4e2f005324d0849e891c3aa24e9
apdaza <apdaza@gmail.com>
2023-08-11 07:12:20 -0500

    paso por valor en 003

---
+++ 003.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void modificar_referencia(int* ptr){
+    *ptr = 10;
+}
+
+void modificar_valor(int valor){
+    //cout << "el valor original es " << valor << endl;
+    valor = 20;
+    //cout << "el valor dentro de la funcion es " << valor << endl;
+}
+
+int main(){
+    int num = 5;
+    cout << "Valor de la variable: " << num << endl;
+    modificar_valor(num);
+    cout << "despues de modificar por valor: " << num << endl;
+
+    return 0;
+}
